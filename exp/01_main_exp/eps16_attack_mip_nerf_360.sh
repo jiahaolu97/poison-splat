1fa53356d1826fc5c9571357718a21fe5b33737e
jiahaolu <jiahaolu97@outlook.com>
2024-09-26 15:21:44 +0800

    mip-nerf-360 eps16 attack script

--- exp/01_main_exp/eps16_attack_mip_nerf_360.sh
+++ exp/01_main_exp/eps16_attack_mip_nerf_360.sh
@@ -1,38 +1,38 @@
 python attacker/poison_splat_bounded.py --gpu 0 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/bicycle/ --decoy_log_path log/attacker_eps16_mip_nerf_360/bicycle/ --data_output_path dataset/MIP_Nerf_360_eps16/bicycle/ &
+    --data_path dataset/MIP_Nerf_360/bicycle/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/bicycle/ --data_output_path dataset/MIP_Nerf_360_eps16/bicycle/ &
 python attacker/poison_splat_bounded.py --gpu 1 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/bonsai/ --decoy_log_path log/attacker_eps16_mip_nerf_360/bonsai/ --data_output_path dataset/MIP_Nerf_360_eps16/bonsai/ &
+    --data_path dataset/MIP_Nerf_360/bonsai/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/bonsai/ --data_output_path dataset/MIP_Nerf_360_eps16/bonsai/ &
 python attacker/poison_splat_bounded.py --gpu 2 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/counter/ --decoy_log_path log/attacker_eps16_mip_nerf_360/counter/ --data_output_path dataset/MIP_Nerf_360_eps16/counter/ &
+    --data_path dataset/MIP_Nerf_360/counter/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/counter/ --data_output_path dataset/MIP_Nerf_360_eps16/counter/ &
 python attacker/poison_splat_bounded.py --gpu 3 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/flowers/ --decoy_log_path log/attacker_eps16_mip_nerf_360/flowers/ --data_output_path dataset/MIP_Nerf_360_eps16/flowers/ &
+    --data_path dataset/MIP_Nerf_360/flowers/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/flowers/ --data_output_path dataset/MIP_Nerf_360_eps16/flowers/ &
 python attacker/poison_splat_bounded.py --gpu 4 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/garden/ --decoy_log_path log/attacker_eps16_mip_nerf_360/garden/ --data_output_path dataset/MIP_Nerf_360_eps16/garden/ &
+    --data_path dataset/MIP_Nerf_360/garden/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/garden/ --data_output_path dataset/MIP_Nerf_360_eps16/garden/ &
 python attacker/poison_splat_bounded.py --gpu 5 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/kitchen/ --decoy_log_path log/attacker_eps16_mip_nerf_360/kitchen/ --data_output_path dataset/MIP_Nerf_360_eps16/kitchen/ &
+    --data_path dataset/MIP_Nerf_360/kitchen/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/kitchen/ --data_output_path dataset/MIP_Nerf_360_eps16/kitchen/ &
 python attacker/poison_splat_bounded.py --gpu 6 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/room/ --decoy_log_path log/attacker_eps16_mip_nerf_360/room/ --data_output_path dataset/MIP_Nerf_360_eps16/room/ &
+    --data_path dataset/MIP_Nerf_360/room/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/room/ --data_output_path dataset/MIP_Nerf_360_eps16/room/ &
 python attacker/poison_splat_bounded.py --gpu 7 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/stump/ --decoy_log_path log/attacker_eps16_mip_nerf_360/stump/ --data_output_path dataset/MIP_Nerf_360_eps16/stump/
+    --data_path dataset/MIP_Nerf_360/stump/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/stump/ --data_output_path dataset/MIP_Nerf_360_eps16/stump/
 wait
 python victim/gaussian-splatting/benchmark.py --gpu 0\
-    -s dataset/MIP_Nerf_360_eps16/bicycle/ -m log/victim_gs_mip_nerf_360_eps16/bicycle/ &
+    -s dataset/MIP_Nerf_360_eps16/bicycle/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/bicycle/ &
 python victim/gaussian-splatting/benchmark.py --gpu 1\
-    -s dataset/MIP_Nerf_360_eps16/bonsai/ -m log/victim_gs_mip_nerf_360_eps16/bonsai/ &
+    -s dataset/MIP_Nerf_360_eps16/bonsai/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/bonsai/ &
 python victim/gaussian-splatting/benchmark.py --gpu 2\
-    -s dataset/MIP_Nerf_360_eps16/counter/ -m log/victim_gs_mip_nerf_360_eps16/counter/ &
+    -s dataset/MIP_Nerf_360_eps16/counter/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/counter/ &
 python victim/gaussian-splatting/benchmark.py --gpu 3\
-    -s dataset/MIP_Nerf_360_eps16/flowers/ -m log/victim_gs_mip_nerf_360_eps16/flowers/ &
+    -s dataset/MIP_Nerf_360_eps16/flowers/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/flowers/ &
 python victim/gaussian-splatting/benchmark.py --gpu 4\
-    -s dataset/MIP_Nerf_360_eps16/garden/ -m log/victim_gs_mip_nerf_360_eps16/garden/ &
+    -s dataset/MIP_Nerf_360_eps16/garden/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/garden/ &
 python victim/gaussian-splatting/benchmark.py --gpu 5\
-    -s dataset/MIP_Nerf_360_eps16/kitchen/ -m log/victim_gs_mip_nerf_360_eps16/kitchen/ &
+    -s dataset/MIP_Nerf_360_eps16/kitchen/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/kitchen/ &
 python victim/gaussian-splatting/benchmark.py --gpu 6\
-    -s dataset/MIP_Nerf_360_eps16/room/ -m log/victim_gs_mip_nerf_360_eps16/room/ &
+    -s dataset/MIP_Nerf_360_eps16/room/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/room/ &
 python victim/gaussian-splatting/benchmark.py --gpu 7\
-    -s dataset/MIP_Nerf_360_eps16/stump/ -m log/victim_gs_mip_nerf_360_eps16/stump/
+    -s dataset/MIP_Nerf_360_eps16/stump/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/stump/
 wait
 python attacker/poison_splat_bounded.py --gpu 0 --adv_epsilon 16 --adv_iters 12000\
-    --data_path dataset/MIP_Nerf_360/treehill/ --decoy_log_path log/attacker_eps16_mip_nerf_360/treehill/ --data_output_path dataset/MIP_Nerf_360_eps16/treehill/ 
+    --data_path dataset/MIP_Nerf_360/treehill/ --decoy_log_path log/01_main_exp/attacker_eps16_mip_nerf_360/treehill/ --data_output_path dataset/MIP_Nerf_360_eps16/treehill/ 
 python victim/gaussian-splatting/benchmark.py --gpu 0\
-    -s dataset/MIP_Nerf_360_eps16/treehill/ -m log/victim_gs_mip_nerf_360_eps16/treehill/
+    -s dataset/MIP_Nerf_360_eps16/treehill/ -m log/01_main_exp/victim_gs_mip_nerf_360_eps16/treehill/
